14ea5cc7c4861a6f0256c6f5c04a37a4649e4f12
wangyh <wangyhzzzz@gmail.com>
2020-12-08 12:16:27 +0800

    readme

--- TaskTitle/script.js
+++ TaskTitle/script.js
@@ -3,13 +3,13 @@ setInterval(function () {
     if (eles.length == 0 || eles.length == 3) return
     var a = document.createElement('a');
     a.className = "url-handler";
-    a.innerHTML = ('复制标题');
-    $('body > div.modal.in > div.object-modal-view.flex-space.flex.flex-center > div > div.object-nav')[0].appendChild(a);
+    a.innerHTML = ('复');
+    $('.object-nav')[0].appendChild(a);
     console.error("添加按钮成功")
     a.onclick = function () {
-        var text = $('body > div.modal.in > div.object-modal-view.flex-space.flex.flex-center > div > div.task-detail.column-rest-fill.modal-detail > div > div > div:nth-child(1) > div > div.detail-head__left__21HR > div.detail-head__object-caption__3OtG > div.default-caption__2C1d > span')[0].innerText;
+        var text = $('.task-id-label')[0].innerText;
         text += ":"
-        text += $('#print_area > div.body-left__3HKz > div > div:nth-child(2) > div > div > div')[0].innerText
+        text += $('.object-content__irzS')[0].innerText
         const input = document.createElement('INPUT');
         input.style.opacity = 0;
         input.style.position = 'absolute';
