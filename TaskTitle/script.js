aa10266b4295a4665f2c2cb4b7e72a9ed84efc33
wangyh <wangyhzzzz@gmail.com>
2020-12-18 15:40:32 +0800

    v1.2

--- TaskTitle/script.js
+++ TaskTitle/script.js
@@ -1,24 +1,29 @@
 setInterval(function () {
-    var eles = $('.url-handler')
-    if (eles.length == 0 || eles.length == 3) return
-    var a = document.createElement('a');
-    a.className = "url-handler";
-    a.innerHTML = ('复');
-    $('.object-nav')[0].appendChild(a);
-    console.error("添加按钮成功")
-    a.onclick = function () {
-        var text = $('.task-id-label')[0].innerText;
-        text += ":"
-        text += $('.object-content__irzS')[0].innerText
-        const input = document.createElement('INPUT');
-        input.style.opacity = 0;
-        input.style.position = 'absolute';
-        input.style.left = '-100000px';
-        document.body.appendChild(input);
-        input.value = text;
-        input.select();
-        input.setSelectionRange(0, text.length);
-        document.execCommand('copy');
-        document.body.removeChild(input);
-    }
+    var label = $('.task-id-label')[0]
+    if(!label)return;
+    if(label.innerText!=label.getAttribute("data-clipboard-text"))return;
+    var desc = $('.object-content__irzS')[0]
+    if(!desc)return;
+    label.setAttribute("data-clipboard-text",label.innerText+":"+desc.innerText)
+    label.style.color='#f00'
+    // var a = document.createElement('a');
+    // a.className = "url-handler";
+    // a.innerHTML = ('复');
+    // $('.object-nav')[0].appendChild(a);
+    // console.error("添加按钮成功")
+    // a.onclick = function () {
+        // var text = $('.task-id-label')[0].innerText;
+        // text += ":"
+        // text += $('.object-content__irzS')[0].innerText
+        // const input = document.createElement('INPUT');
+        // input.style.opacity = 0;
+        // input.style.position = 'absolute';
+        // input.style.left = '-100000px';
+        // document.body.appendChild(input);
+        // input.value = text;
+        // input.select();
+        // input.setSelectionRange(0, text.length);
+        // document.execCommand('copy');
+        // document.body.removeChild(input);
+    // }
 }, 1000)
--- readme.txt
+++ readme.txt
@@ -1,4 +1,5 @@
 1. 运行TaskTitle.reg
 2. 重启谷歌浏览器
-3. 进入谷歌开发者插件设置，打开开发者模式
-4. 把TaskTitle.crx拖入谷歌浏览器中
+3. 进入谷歌开发者插件设置，打开开发者模式 (地址栏输入chrome://extensions/)
+4. 把TaskTitle.crx拖入谷歌浏览器中,安装
+5. 重启谷歌浏览器
